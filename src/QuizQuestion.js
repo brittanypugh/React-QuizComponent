990d2d82a567a5e72954d0561ab60bbb0e3f70ae
Brittany Pugh <pughbritt00@gmail.com>
2021-05-26 01:57:53 -0500

    create new QuizQuestion Component

---
+++ src/QuizQuestion.js
@@ -0,0 +1,20 @@
+import React, {Compoennt} from 'react'
+
+class QuizQuestion extends React.Component {
+    render(){
+        return (
+            <main>
+                <section>
+                    <p>// instruction text goes here</p></section>
+                <section className="buttons">
+                    <ul>
+                        //quiz question button logic goes here
+                    </ul>
+                </section>
+            </main>    
+        
+        )
+    }
+}
+
+export default QuizQuestion
